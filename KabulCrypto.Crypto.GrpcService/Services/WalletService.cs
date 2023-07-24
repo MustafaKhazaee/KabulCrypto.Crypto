53d57b58a582fb1a7a76eeebb4937c4aff76ea5a
Mustafa Khazaee <101381823+MustafaKhazaee@users.noreply.github.com>
2023-07-24 21:40:44 +0430

    save

--- KabulCrypto.Crypto.GrpcService/Services/WalletService.cs
+++ KabulCrypto.Crypto.GrpcService/Services/WalletService.cs
@@ -1,7 +1,6 @@
 ﻿
 using Grpc.Core;
 using KabulCrypto.Crypto.Application.Queries;
-using KabulCrypto.Crypto.Domain.Interfaces;
 using MediatR;
 
 namespace KabulCrypto.Crypto.GrpcService.Services;
@@ -9,12 +8,10 @@ namespace KabulCrypto.Crypto.GrpcService.Services;
 public class WalletService : Wallet.WalletBase
 {
     private readonly IMediator _mediator;
-    private readonly IApplicationDbContext  _context;
 
-    public WalletService(IMediator sender, IApplicationDbContext context)
+    public WalletService(IMediator sender)
     {
         _mediator = sender;
-        _context = context;
     }
 
     public async override Task<GetWalletsResponse> GetWallets(GetWalletsRequest request, ServerCallContext context)
